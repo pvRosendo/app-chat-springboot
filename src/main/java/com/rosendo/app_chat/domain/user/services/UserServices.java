867683a6f93091cfc91a963ad5f351a0cce8e2d9
Paulo Vitor Rosendo <paulo.vitor.rsd@poli.ufrj.br>
2024-06-18 01:46:05 -0300

    feat: add entity user and permission with basic of auth

---
+++ src/main/java/com/rosendo/app_chat/domain/user/services/UserServices.java
@@ -0,0 +1,86 @@
+package com.rosendo.app_chat.domain.user.services;
+
+import com.rosendo.app_chat.domain.user.dtos.CreateAccountCredentialsDto;
+import com.rosendo.app_chat.domain.user.models.User;
+import com.rosendo.app_chat.domain.user.repositories.UserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserServices implements UserDetailsService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserServices(UserRepository repository) {
+        this.userRepository = repository;
+    }
+
+    @Transactional
+    public CreateAccountCredentialsDto createUser(CreateAccountCredentialsDto credentials) {
+        var user = new User();
+        var passwordEncoded = passwordEncoder(credentials.password());
+
+        user.setUserName(credentials.username());
+        user.setPassword(passwordEncoded);
+        user.setAccountNonExpired(true);
+        user.setAccountNonLocked(true);
+        user.setCredentialsNonExpired(true);
+        user.setEnabled(true);
+
+        userRepository.save(user);
+
+        setUserPermissions(user.getId(), credentials.permission());
+        return credentials;
+    }
+
+    @Transactional
+    public void setUserPermissions(long userId, long permissionId) {
+        String sql = "INSERT INTO tb_user_permission (id_user, id_permission) VALUES (:userId, :permissionId)";
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("userId", userId);
+        query.setParameter("permissionId", permissionId);
+        query.executeUpdate();
+    }
+
+    public String passwordEncoder(String password) {
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+
+        Pbkdf2PasswordEncoder pbkdf2Encoder =
+                new Pbkdf2PasswordEncoder(
+                        "", 8, 185000,
+                        Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+
+        encoders.put("pbkdf2", pbkdf2Encoder);
+        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
+
+        return passwordEncoder.encode(password).substring(8);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username);
+        if (user != null) {
+            return user;
+        } else {
+            throw new UsernameNotFoundException("Username " + username + " not found!");
+        }
+    }
+}
