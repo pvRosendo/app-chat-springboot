ce2ccdc58fdcb57f0f6ae0ba5462ba651c2686db
Paulo Vitor Rosendo <paulo.vitor.rsd@poli.ufrj.br>
2024-06-17 21:30:06 -0300

    feat: change the structure for use rabbitmq

--- src/main/java/com/rosendo/app_chat/infrastructure/config/WebSocketChatConfig.java
+++ src/main/java/com/rosendo/app_chat/infrastructure/config/WebSocketChatConfig.java
@@ -2,22 +2,22 @@ package com.rosendo.app_chat.infrastructure.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+public class WebSocketChatConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/websocket").withSockJS();
+    registry.addEndpoint("/websocketApp").withSockJS();
   }
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
-    registry.enableSimpleBroker("/topic");
     registry.setApplicationDestinationPrefixes("/app");
+    registry.enableStompBrokerRelay("/topic").setRelayHost("localhost").setRelayPort(61613).setClientLogin("guest")
+            .setClientPasscode("guest");
+
   }
 }
