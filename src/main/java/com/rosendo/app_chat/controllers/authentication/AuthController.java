c2b4676691b0afd65a8cb0a326057ef0ccb72a6d
Paulo Vitor Rosendo <paulo.vitor.rsd@poli.ufrj.br>
2024-06-18 01:48:16 -0300

    feat: implements services and controller for auth

---
+++ src/main/java/com/rosendo/app_chat/controllers/authentication/AuthController.java
@@ -0,0 +1,47 @@
+package com.rosendo.app_chat.controllers.authentication;
+
+import com.rosendo.app_chat.domain.user.dtos.AccountCredentialsDto;
+import com.rosendo.app_chat.domain.user.dtos.CreateAccountCredentialsDto;
+import com.rosendo.app_chat.domain.user.services.AuthServices;
+import com.rosendo.app_chat.domain.user.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    AuthServices authServices;
+
+    @Autowired
+    UserServices userServices;
+
+    @PostMapping(value = "/user")
+    public ResponseEntity<?> createAccount(@RequestBody CreateAccountCredentialsDto data) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userServices.createUser(data));
+    }
+
+    @SuppressWarnings("rawtypes")
+    @PostMapping(value = "/signin")
+    public ResponseEntity signin(@RequestBody AccountCredentialsDto data) {
+        if (authServices.checkIfParamsIsNotNull(data))
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+        var token = authServices.signin(data);
+        if (token == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+        return token;
+    }
+
+    @SuppressWarnings("rawtypes")
+    @PutMapping(value = "/refresh/{username}")
+    public ResponseEntity refreshToken(@PathVariable("username") String username, @RequestHeader("Authorization") String refreshToken) {
+        if (authServices.checkIfParamsIsNotNull(username, refreshToken))
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+        var token = authServices.refreshToken(username, refreshToken);
+        if (token == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+        return token;
+    }
+}
---
+++ src/main/java/com/rosendo/app_chat/domain/user/services/AuthServices.java
@@ -0,0 +1,70 @@
+package com.rosendo.app_chat.domain.user.services;
+
+import com.rosendo.app_chat.domain.user.dtos.AccountCredentialsDto;
+import com.rosendo.app_chat.domain.user.dtos.TokenDto;
+import com.rosendo.app_chat.domain.user.repositories.UserRepository;
+import com.rosendo.app_chat.infrastructure.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServices {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private UserRepository repository;
+
+    @SuppressWarnings("rawtypes")
+    public ResponseEntity signin(AccountCredentialsDto data) {
+        try {
+            var username = data.username();
+            var password = data.password();
+
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+            var user = repository.findByUsername(username);
+
+            var tokenResponse = new TokenDto();
+            if (user != null) {
+                tokenResponse = tokenProvider.createAccessToken(username, user.getRoles());
+            } else {
+                throw new UsernameNotFoundException("Username " + username + " not found!");
+            }
+            return ResponseEntity.ok(tokenResponse);
+        } catch (Exception e) {
+            throw new BadCredentialsException("Invalid username/password supplied!");
+        }
+    }
+
+    @SuppressWarnings("rawtypes")
+    public ResponseEntity refreshToken(String username, String refreshToken) {
+        var user = repository.findByUsername(username);
+
+        var tokenResponse = new TokenDto();
+        if (user != null) {
+            tokenResponse = tokenProvider.refreshToken(refreshToken);
+        } else {
+            throw new UsernameNotFoundException("Username " + username + " not found!");
+        }
+        return ResponseEntity.ok(tokenResponse);
+    }
+
+    public boolean checkIfParamsIsNotNull(String username, String refreshToken) {
+        return refreshToken == null || refreshToken.isBlank() || username == null || username.isBlank();
+    }
+
+    public boolean checkIfParamsIsNotNull(AccountCredentialsDto data) {
+        return data == null || data.username() == null || data.username().isBlank()
+                || data.password() == null || data.password().isBlank();
+    }
+}
