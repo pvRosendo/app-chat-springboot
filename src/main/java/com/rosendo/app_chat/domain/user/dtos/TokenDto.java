f885cf901c2aa7206df8a6cd44f59a7a09f244cb
Paulo Vitor Rosendo <paulo.vitor.rsd@poli.ufrj.br>
2024-06-18 01:46:47 -0300

    feat: implements the create and refresh tokens with jwt

---
+++ src/main/java/com/rosendo/app_chat/domain/user/dtos/TokenDto.java
@@ -0,0 +1,27 @@
+package com.rosendo.app_chat.domain.user.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+
+public record TokenDto(
+        @NotBlank String username,
+        @NotNull Boolean authenticated,
+        @NotBlank Date created,
+        @NotBlank Date expiration,
+        @NotBlank String accessToken,
+        @NotBlank String refreshToken
+) {
+    public TokenDto() {
+        this("defaultName",
+                false,
+                Date.from(Instant.now()),
+                Date.from(Instant.now().plus(Duration.ofHours(2))),
+                "",
+                ""
+        );
+    }
+}
